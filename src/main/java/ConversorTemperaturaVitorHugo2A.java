1eca1845cbc444b0129ca0aaaf749019a52a0bfa
vitor Hugo m. Vanzella <vitormoreiravanzella@gmail.com>
2025-04-29 09:41:44 -0300

    primeiro Commit

---
+++ src/main/java/ConversorTemperaturaVitorHugo2A.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author 2024105070030
+ */
+public class ConversorTemperaturaVitorHugo2A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
